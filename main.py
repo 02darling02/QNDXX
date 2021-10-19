ee20c35deb8856d79ad13afa15be81f89c3e614c
“chiupam” <chiupam@126.com>
2021-10-19 16:07:39 +0800

    修复

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import json
 import time
 
 
-def everyWeek(cookie):
+def everyWeek(cookie, id):
     """
     每周青年大学习
     :param cookie: 传入 Cookie
@@ -15,20 +15,11 @@ def everyWeek(cookie):
     task = '每周青年大学习\n'
     url = 'http://home.yngqt.org.cn/qndxx/xuexi.ashx'
     headers = {
-        "Host": "home.yngqt.org.cn",
-        "Accept": "application/json, text/javascript, */*; q=0.01",
-        "X-Requested-With": "XMLHttpRequest",
-        "Accept-Language": "zh-cn",
-        "Accept-Encoding": "gzip, deflate",
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-        "Origin": "http://home.yngqt.org.cn",
-        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.4(0x1800042c) NetType/4G Language/zh_CN",
-        "Referer": "http://home.yngqt.org.cn/qndxx/",
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_5_1 like Mac OS X) \
+                       AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.4(0x1800042c) NetType/4G Language/zh_CN",
         "Cookie": cookie
     }
-    data = {
-        "txtid": time.localtime()[7] // 7 + 45
-    }
+    data = {"txtid": id}
     try:
         r = requests.post(url, data=json.dumps(data), headers=headers, proxies={"http": None, "https": None})
         r.encoding = r.apparent_encoding
@@ -57,16 +48,8 @@ def everyDay(cookie):
     task = '每日签到领积分\n'
     url = "http://home.yngqt.org.cn/qndxx/user/qiandao.ashx"
     headers = {
-        "Host": "home.yngqt.org.cn",
-        "X-Requested-With": "XMLHttpRequest",
-        "Accept-Encoding": "gzip, deflate",
-        "Accept-Language": "zh-cn",
-        "Accept": "application/json, text/javascript, */*; q=0.01",
-        "Origin": "http://home.yngqt.org.cn",
-        "Content-Length": "0",
-        "Connection": "keep-alive",
-        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.4(0x1800042c) NetType/4G Language/zh_CN",
-        "Referer": "http://home.yngqt.org.cn/qndxx/user/guize.aspx",
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_5_1 like Mac OS X) \
+                       AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.4(0x1800042c) NetType/4G Language/zh_CN",
         "Cookie": cookie
     }
     try:
@@ -101,7 +84,8 @@ def main(event, context):
         if time.localtime()[6] in [0, 1, 2]:  # 周一、周二、周三各学习一遍
             i = 3
             while i:
-                result = everyWeek(cookie)
+                id = time.localtime()[7] // 7 + 44
+                result = everyWeek(cookie, id)
                 if result:
                     i = 0
                 else:
