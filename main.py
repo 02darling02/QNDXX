767b2014be02e9e753d027ed17eb6171b0924a49
chiupam <chiupam@126.com>
2021-11-08 15:47:19 +0800

    优化

--- main.py
+++ main.py
@@ -6,23 +6,11 @@ from requests import post
 from json import dumps, load
 
 
-def sign(task, cookie):
-    url = f"http://home.yngqt.org.cn/qndxx/{task}.ashx"
-    headers = {
-        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, "
-                      "like Gecko) Mobile/15E148 MicroMessenger/8.0.4(0x1800042c) NetType/4G Language/zh_CN",
-        "Cookie": cookie
-    }
-    res = post(url, data=dumps({"txtid": localtime()[7] // 7 + 44}), headers=headers) if task == "xuexi" else post(url, headers=headers)
-    res.encoding = res.apparent_encoding
-    print(res.json())
-
-
 def main(event, context):
     with open('./cookie.json', 'r', encoding='utf-8') as f:
         for cookie in load(f):
-            sign("xuexi", cookie)
-            sign("user/qiandao", cookie)
+            print(post(url="http://home.yngqt.org.cn/qndxx/xuexi.ashx", data=dumps({"txtid": localtime()[7] // 7 + 44}), headers={"Cookie": cookie}).text)
+            print(post(url="http://home.yngqt.org.cn/qndxx/user/qiandao.ashx", headers={"Cookie": cookie}).text)
 
 
 if __name__ == '__main__':
