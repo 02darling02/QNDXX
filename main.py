0c0420f85f3d8cadbbb09c88205da389c41c56cb
chiupam <chiupam@126.com>
2021-11-08 16:04:05 +0800

    优化

--- main.py
+++ main.py
@@ -4,10 +4,11 @@
 from time import localtime
 from requests import post
 from json import dumps, load
+from os.path import exists, abspath, dirname
 
 
 def main(event, context):
-    with open('./cookie.json', 'r', encoding='utf-8') as f:
+    with open("/data/cookie.json" if exists("/data/cookie.json") else f"{dirname(abspath(__file__))}/cookie.json", 'r', encoding='utf-8') as f:
         for cookie in load(f):
             print(post(url="http://home.yngqt.org.cn/qndxx/xuexi.ashx", data=dumps({"txtid": localtime()[7] // 7 + 44}), headers={"Cookie": cookie}).text)
             print(post(url="http://home.yngqt.org.cn/qndxx/user/qiandao.ashx", headers={"Cookie": cookie}).text)
