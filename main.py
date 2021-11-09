d13dcbc2e477445de7399543c4e38ec84830aa1a
chiupam <chiupam@126.com>
2021-11-09 15:37:07 +0800

    首次提交

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from os.path import exists, abspath, dirname
 def main(event, context):
     with open("/data/cookie.json" if exists("/data/cookie.json") else f"{dirname(abspath(__file__))}/cookie.json", 'r', encoding='utf-8') as f:
         for cookie in load(f):
-            get(url="http://home.yngqt.org.cn/qndxx/switch.aspx", headers={"Cookie": cookie})
             get(url="http://home.yngqt.org.cn/qndxx/default.aspx", headers={"Cookie": cookie})
             print(post(url="http://home.yngqt.org.cn/qndxx/xuexi.ashx", data=dumps({"txtid": localtime()[7] // 7 + 44}), headers={"Cookie": cookie}).text)
             print(post(url="http://home.yngqt.org.cn/qndxx/user/qiandao.ashx", headers={"Cookie": cookie}).text)
